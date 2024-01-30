327784bd9679db129445d092dd7280e2406144bf
zhang-xiaohang <customizationprogram@gmail.com>
2024-01-30 17:38:27 +0800

    preload

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
-
+  win.webContents.openDevTools()
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => "pong")
   createWindow()
 })
